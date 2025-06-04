51d0c5bb3c359f807335ccd3b8ad5dfd730bc266
SapunArtem <sapun_artem@mail.ru>
2025-06-04 10:45:00 +0300

    create activity_calculator.xml

--- CalculatorApp/build.gradle.kts
+++ CalculatorApp/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.calculatorapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.calculatorapp"
